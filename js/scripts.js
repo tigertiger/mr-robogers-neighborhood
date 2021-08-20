6f8535bce22707fec8635538c68b43dfe2fda043
Melissa <melissa.scmi@gmail.com>
2021-08-20 16:27:36 -0700

    remove comments & WIP code

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,6 @@ function beepBoop(input) {
   let bbArray = [];
   for (let i = 0; i <= input; i++) {
     let numbers = i.toString();
-    console.log(numbers);
     if (numbers.includes(3)) {
       bbArray.push("Won't you be my neighbor?");
     } else if (numbers.includes(2)) {
@@ -25,40 +24,5 @@ $("#bbCounter").submit(function(event) {
 event.preventDefault();
 const finalNumber = $("#ourNumber").val();
 $("#counterBlock").text(beepBoop(finalNumber));
-// let numberList = (beepBoop(finalNumber));
-// console.log(numberList);
-// $("#test").text(numberList[0] + " " + numberList[1]);
-// testing
-
-//
-
 })
 });
-
-// Working
-// $(document).ready(function() {
-//   $("#bbCounter").submit(function(event) {
-//   event.preventDefault();
-//   const finalNumber = $("#ourNumber").val();
-//   $("#counterBlock").text(beepBoop(finalNumber));
-//   })
-//   });
-
-// Test
-
-// function beepBoop(input) {
-//   let bbArray = [];
-//   let numbers = [input.toString()]
-//   numbers.forEach(function(number) {
-//     if (number.includes(3)) {
-//       bbArray.push("Won't you be my neighbor?");
-//     } else if (number.includes(2)) {
-//       bbArray.push("Boop!");
-//     } else if (number.includes(1)) {
-//       bbArray.push("Beep!");
-//     } else {
-//     bbArray.push(number[i]);
-//     }
-//   })
-//   return bbArray;
-// }
